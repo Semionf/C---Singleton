280cddb51a3a4b4ed495613652ec563f35d99f45
semionf <sfurlender@gmail.com>
2022-11-24 12:05:56 +0200

    .

---
+++ Singleton/Single.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Single
+    {
+        
+        
+        public static readonly Single Instance = new Single();
+        private Single() { }
+
+      
+        
+    }
+  
+
+}
+
